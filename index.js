b73524b257034836e4235926613e33aaefd66345
Diaconu Dan <diaconudann@gmail.com>
2019-02-28 22:06:11 +0100

    Added accessToken

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientId,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken)=> {
-    console.log(accessToken)
+}, (accessToken, refreshToken, profile, done)=> {
+    console.log('accessToken:' + accessToken);
+    console.log(refreshToken);
+    console.log('profile:' + JSON.stringify(profile));
 }));
 
 
@@ -20,6 +22,9 @@ app.get('/auth/google',
         scope:['profile','email']
     })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT);
