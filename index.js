1e1a7ce86644913fdff4860762b33a34f09793a2
Diaconu Dan <diaconudann@gmail.com>
2019-03-05 22:04:30 +0100

    Add user on MongoDB after google auth success

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./server/config/keys');
+require('./server/models/User');
 require('./server/services/passport');
 
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
+
 
 require('./server/routes/authRoutes')(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy ;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 //clientID and clientSecret generated in keys
 passport.use(new GoogleStrategy({
@@ -8,7 +11,5 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done)=> {
-    console.log('accessToken:' + accessToken);
-    console.log(refreshToken);
-    console.log('profile:' + JSON.stringify(profile));
+    new User({ googleId: profile.id}).save();
 }));
